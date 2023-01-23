691a56445a6586bc2a806f2b217f049d5c920f46
anagdinizp <anagabriela0399@hotmail.com>
2023-01-23 10:56:38 -0300

    refactor: refactoring the code

--- src/redux/reducers/dataReducer.tsx
+++ src/redux/reducers/dataReducer.tsx
@@ -2,37 +2,36 @@ import { createSlice } from "@reduxjs/toolkit";
 import { ICard, IChart } from "../../components/organisms/dashboard";
 
 interface IData {
-  currentSearch: ICard;
-  recentSearchList: ICard[];
-  chartPoints: IChart[];
-  isLoading: boolean;
+  currentSearch: ICard; // Dados gerais de uma empresa 
+  recentSearchList: ICard[]; // Lista com todas as empresas recentemente pesquisadas
+  chartPoints: IChart[]; // Valores referentes a data e valor da ação que vão ser utilizados para formar o eixo x e y do gráfico
+  isLoading: boolean; // Booleano para utilizar o loading durante o ato de pesquisar
 }
 
-// Estado inicial dos dados que vão ser recebidos
 const initialState: IData = {
   currentSearch: {
-    companyName: "", // Nome da empresa
-    latestPrice: 0, // O valor mais atual da ação dessa empresa
-    symbol: "", // Símbolo que representa o nome da empresa, por exemplo, AAPL para Apple
-    isFav: false, // Booleano para verificar se é uma empresa favorita pelo usuário
-    diffRange: 0, // O range é a diferença calculada do intervalo de tempo de um mês das ações da empresa, pegando o primeiro e o ultimo valor para definir, dependendo do resultado, se a ação da empresa está subindo ou descendo
-    percentageRange: 0, // É o mesmo valor calculado pelo diffRange só que em porcentagem
+    companyName: "", 
+    latestPrice: 0, 
+    symbol: "", 
+    isFav: false, 
+    diffRange: 0, 
+    percentageRange: 0, 
   },
-  recentSearchList: [], // Lista de empresas recentemente pesquisadas
-  chartPoints: [], // Valores referentes a data e valor da ação que vão ser utilizados para formar o eixo x e y do gráfico
-  isLoading: false, // Booleano para utilizar o loading durante o ato de pesquisar
+  recentSearchList: [],
+  chartPoints: [],
+  isLoading: false, 
 };
 
 export const dataReducer = createSlice({
   name: "data",
   initialState,
   reducers: {
-    // Função que atualiza a lista de empresas favoritadas
     updateFavList: (state, action: { payload: { quote: ICard } }) => {
       const { quote } = action.payload;
-      const newFavList = state.recentSearchList.map((fav) =>
-        fav.symbol !== quote.symbol ? fav : { ...fav, isFav: !quote.isFav }
+      const newFavList = state.recentSearchList.map((action) =>
+        action.symbol !== quote.symbol ? action : { ...action, isFav: !quote.isFav } 
       );
+
       if (state.currentSearch.symbol === quote.symbol)
         state.currentSearch = { ...quote, isFav: !quote.isFav };
       state.recentSearchList = newFavList;
@@ -54,12 +53,12 @@ export const dataReducer = createSlice({
 
       const percentageRange =
         (diffRange / Number(data[data.length - 1]?.uv)) * 100;
-
+      
       const isFav =
         state.recentSearchList.find(
           (quote: ICard) => quote.symbol === companyInfo.symbol
         )?.isFav ?? false;
-
+    
       const currentSearch: ICard = {
         latestPrice: companyInfo.latestPrice,
         companyName: companyInfo.companyName.split(" ")[0],
